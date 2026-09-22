3c948876e40503178a436ec533d7c163e1aeda8f
Andrew <zevs777@mail.ru>
2026-09-22 20:05:32 +0300

    fix: bind vite dev server to ipv4

--- vite.config.ts
+++ vite.config.ts
@@ -10,8 +10,9 @@ export default defineConfig({
     },
   },
   server: {
+    host: '127.0.0.1',
     proxy: {
-      '/api': 'http://localhost:3000',
+      '/api': 'http://127.0.0.1:3000',
     },
   },
   test: {
