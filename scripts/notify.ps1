dc5b46786f467f4843c74a280979d24f59802a59
Andrew <zevs777@mail.ru>
2026-09-24 22:49:46 +0300

    docs: add Windows toast notification workflow and helper script

---
+++ scripts/notify.ps1
@@ -0,0 +1,12 @@
+﻿param(
+  [Parameter(Mandatory = $true)][string]$Title,
+  [Parameter(Mandatory = $true)][string]$Message
+)
+
+[Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] | Out-Null
+$template = [Windows.UI.Notifications.ToastNotificationManager]::GetTemplateContent([Windows.UI.Notifications.ToastTemplateType]::ToastText02)
+$texts = $template.GetElementsByTagName('text')
+$texts.Item(0).AppendChild($template.CreateTextNode($Title)) | Out-Null
+$texts.Item(1).AppendChild($template.CreateTextNode($Message)) | Out-Null
+$toast = [Windows.UI.Notifications.ToastNotification]::new($template)
+[Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier('Календарь звонков').Show($toast)
